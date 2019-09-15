4b98a45fcbc13b0dce010e65312aa711203b566f
Tamas K. Papp <tkpapp@gmail.com>
2019-09-15 09:24:08 +0200

    Initial LogExpFunctions commit.
    This is a straightforward copy from StatsFuns.jl.

---
+++ README.md
@@ -0,0 +1,14 @@
+# LogExpFunctions.jl
+
+![Lifecycle](https://img.shields.io/badge/lifecycle-experimental-orange.svg)<!--
+![Lifecycle](https://img.shields.io/badge/lifecycle-maturing-blue.svg)
+![Lifecycle](https://img.shields.io/badge/lifecycle-stable-green.svg)
+![Lifecycle](https://img.shields.io/badge/lifecycle-retired-orange.svg)
+![Lifecycle](https://img.shields.io/badge/lifecycle-archived-red.svg)
+![Lifecycle](https://img.shields.io/badge/lifecycle-dormant-blue.svg) -->
+[![Build Status](https://travis-ci.com/tpapp/LogExpFunctions.jl.svg?branch=master)](https://travis-ci.com/tpapp/LogExpFunctions.jl)
+[![codecov.io](http://codecov.io/github/tpapp/LogExpFunctions.jl/coverage.svg?branch=master)](http://codecov.io/github/tpapp/LogExpFunctions.jl?branch=master)
+
+Various special functions based on `log` and `exp` moved from [StatsFuns.jl](https://github.com/JuliaStats/StatsFuns.jl) into a separate package, to minimize dependencies. These functions only use native Julia code, so there is no need to depend on `librmath` or similar libraries. See the discussion at [StatsFuns.jl#46](https://github.com/JuliaStats/StatsFuns.jl/issues/46).
+
+The original authors of these functions are the StatsFuns.jl contributors.
--- src/LogExpFunctions.jl
+++ src/LogExpFunctions.jl
@@ -1,49 +1,43 @@
-# common facilities
+module LogExpFunctions
 
-# scalar functions
+export xlogx, xlogy, logistic, logit, log1psq, log1pexp, log1mexp, log2mexp, logexpm1,
+    log1pmx, logmxp1, logaddexp, logsumexp, softmax!, softmax
+
+using Base: Math.@horner, @irrational
+
+####
+#### constants
+####
+
+@irrational loghalf -0.6931471805599453094 log(big(0.5))
+@irrational logtwo 0.6931471805599453094 log(big(2.))
+@irrational logπ   1.1447298858494001741 log(big(π))
+@irrational log2π  1.8378770664093454836 log(big(2.)*π)
+@irrational log4π  2.5310242469692907930 log(big(4.)*π)
+
+####
+#### functions
+####
+
 """
-    xlogx(x::Number)
+    xlogx(x::Real)
 
-Compute `x * log(x)`, returning zero if `x` is zero.
+Return `x * log(x)` for `x ≥ 0`, handling `x = 0` by taking the downward limit.
 
 ```jldoctest
 julia> StatsFuns.xlogx(0)
 0.0
 ```
 """
-function xlogx(x::Number)
-    result = x * log(x)
-    ifelse(iszero(x), zero(result), result)
-end
+xlogx(x::Real) = x > zero(x) ? x * log(x) : zero(log(x))
 
 """
-    xlogy(x::Number, y::Number)
+    xlogy(x::Real, y::Real)
 
-Compute `x * log(y)`, returning zero if `x` is zero.
-
-```jldoctest
-julia> StatsFuns.xlogy(0, 0)
-0.0
-```
+Return `x * log(y)` for `y > 0` with correct limit at `x = 0`.
 """
-function xlogy(x::Number, y::Number)
-    result = x * log(y)
-    ifelse(iszero(x) && !isnan(y), zero(result), result)
-end
-
-# The following bounds are precomputed versions of the following abstract
-# function, but the implicit interface for AbstractFloat doesn't uniformly
-# enforce that all floating point types implement nextfloat and prevfloat.
-# @inline function _logistic_bounds(x::AbstractFloat)
-#     (
-#         logit(nextfloat(zero(float(x)))),
-#         logit(prevfloat(one(float(x)))),
-#     )
-# end
-
-@inline _logistic_bounds(x::Float16) = (Float16(-16.64), Float16(7.625))
-@inline _logistic_bounds(x::Float32) = (-103.27893f0, 16.635532f0)
-@inline _logistic_bounds(x::Float64) = (-744.4400719213812, 36.7368005696771)
+xlogy(x::T, y::T) where {T<:Real} = x > zero(T) ? x * log(y) : zero(log(x))
+xlogy(x::Real, y::Real) = xlogy(promote(x, y)...)
 
 """
     logistic(x::Real)
@@ -56,20 +50,6 @@ The [logistic](https://en.wikipedia.org/wiki/Logistic_function) sigmoid function
 Its inverse is the [`logit`](@ref) function.
 """
 logistic(x::Real) = inv(exp(-x) + one(x))
-
-function logistic(x::Union{Float16, Float32, Float64})
-    e = exp(x)
-    lower, upper = _logistic_bounds(x)
-    ifelse(
-        x < lower,
-        zero(x),
-        ifelse(
-            x > upper,
-            one(x),
-            e / (one(x) + e)
-        )
-    )
-end
 
 """
     logit(p::Real)
@@ -206,117 +186,37 @@ end
 
 Return `log(exp(x) + exp(y))`, avoiding intermediate overflow/undeflow, and handling non-finite values.
 """
-function logaddexp(x::Real, y::Real)
-    # ensure Δ = 0 if x = y = ± Inf
-    Δ = ifelse(x == y, zero(x - y), abs(x - y))
-    max(x, y) + log1pexp(-Δ)
+function logaddexp(x::T, y::T) where T<:Real
+    # x or y is  NaN  =>  NaN
+    # x or y is +Inf  => +Inf
+    # x or y is -Inf  => other value
+    isfinite(x) && isfinite(y) || return max(x,y)
+    x > y ? x + log1p(exp(y - x)) : y + log1p(exp(x - y))
 end
+logaddexp(x::Real, y::Real) = logaddexp(promote(x, y)...)
 
-Base.@deprecate logsumexp(x::Real, y::Real) logaddexp(x, y)
-
-"""
-    logsubexp(x, y)
-
-Return `log(abs(e^x - e^y))`, preserving numerical accuracy.
-"""
-logsubexp(x::Real, y::Real) = max(x, y) + log1mexp(-abs(x - y))
+Base.@deprecate logsumexp(x::Real, y::Real) logaddexp(x,y)
 
 """
     logsumexp(X)
 
-Compute `log(sum(exp, X))` in a numerically stable way that avoids intermediate over- and
-underflow.
+Compute `log(sum(exp, X))`, evaluated avoiding intermediate overflow/undeflow.
 
-`X` should be an iterator of real numbers. The result is computed using a single pass over
-the data.
-
-# References
-
-[Sebastian Nowozin: Streaming Log-sum-exp Computation.](http://www.nowozin.net/sebastian/blog/streaming-log-sum-exp-computation.html)
+`X` should be an iterator of real numbers.
 """
-logsumexp(X) = _logsumexp_onepass(X)
-
-"""
-    logsumexp(X::AbstractArray{<:Real}; dims=:)
-
-Compute `log.(sum(exp.(X); dims=dims))` in a numerically stable way that avoids
-intermediate over- and underflow.
-
-The result is computed using a single pass over the data.
-
-# References
-
-[Sebastian Nowozin: Streaming Log-sum-exp Computation.](http://www.nowozin.net/sebastian/blog/streaming-log-sum-exp-computation.html)
-"""
-logsumexp(X::AbstractArray{<:Real}; dims=:) = _logsumexp(X, dims)
-
-_logsumexp(X::AbstractArray{<:Real}, ::Colon) = _logsumexp_onepass(X)
-function _logsumexp(X::AbstractArray{<:Real}, dims)
-    # Do not use log(zero(eltype(X))) directly to avoid issues with ForwardDiff (#82)
-    FT = float(eltype(X))
-    xmax_r = reduce(_logsumexp_onepass_op, X; dims=dims, init=(FT(-Inf), zero(FT)))
-    return @. first(xmax_r) + log1p(last(xmax_r))
-end
-
-function _logsumexp_onepass(X)
-    # fallback for empty collections
+function logsumexp(X)
     isempty(X) && return log(sum(X))
-    return _logsumexp_onepass_result(_logsumexp_onepass_reduce(X, Base.IteratorEltype(X)))
+    reduce(logaddexp, X)
 end
-
-# function barrier for reductions with single element and without initial element
-_logsumexp_onepass_result(x) = float(x)
-_logsumexp_onepass_result((xmax, r)::Tuple) = xmax + log1p(r)
-
-# iterables with known element type
-function _logsumexp_onepass_reduce(X, ::Base.HasEltype)
-    # do not perform type computations if element type is abstract
-    T = eltype(X)
-    isconcretetype(T) || return _logsumexp_onepass_reduce(X, Base.EltypeUnknown())
-
-    FT = float(T)
-    return reduce(_logsumexp_onepass_op, X; init=(FT(-Inf), zero(FT)))
-end
-
-# iterables without known element type
-_logsumexp_onepass_reduce(X, ::Base.EltypeUnknown) = reduce(_logsumexp_onepass_op, X)
-
-## Reductions for one-pass algorithm: avoid expensive multiplications if numbers are reduced
-
-# reduce two numbers
-function _logsumexp_onepass_op(x1, x2)
-    a = x1 == x2 ? zero(x1 - x2) : -abs(x1 - x2)
-    xmax = x1 > x2 ? oftype(a, x1) : oftype(a, x2)
-    r = exp(a)
-    return xmax, r
-end
-
-# reduce a number and a partial sum
-function _logsumexp_onepass_op(x, (xmax, r)::Tuple)
-    a = x == xmax ? zero(x - xmax) : -abs(x - xmax)
-    if x > xmax
-        _xmax = oftype(a, x)
-        _r = (r + one(r)) * exp(a)
-    else
-        _xmax = oftype(a, xmax)
-        _r = r + exp(a)
+function logsumexp(X::AbstractArray{T}) where {T<:Real}
+    isempty(X) && return log(zero(T))
+    u = maximum(X)
+    isfinite(u) || return float(u)
+    let u=u # avoid https://github.com/JuliaLang/julia/issues/15276
+        u + log(sum(x -> exp(x-u), X))
     end
-    return _xmax, _r
 end
-_logsumexp_onepass_op(xmax_r::Tuple, x) = _logsumexp_onepass_op(x, xmax_r)
 
-# reduce two partial sums
-function _logsumexp_onepass_op((xmax1, r1)::Tuple, (xmax2, r2)::Tuple)
-    a = xmax1 == xmax2 ? zero(xmax1 - xmax2) : -abs(xmax1 - xmax2)
-    if xmax1 > xmax2
-        xmax = oftype(a, xmax1)
-        r = r1 + (r2 + one(r2)) * exp(a)
-    else
-        xmax = oftype(a, xmax2)
-        r = r2 + (r1 + one(r1)) * exp(a)
-    end
-    return xmax, r
-end
 
 """
     softmax!(r::AbstractArray, x::AbstractArray)
@@ -349,3 +249,5 @@ Return the [`softmax transformation`](https://en.wikipedia.org/wiki/Softmax_func
 """
 softmax!(x::AbstractArray{<:AbstractFloat}) = softmax!(x, x)
 softmax(x::AbstractArray{<:Real}) = softmax!(similar(x, Float64), x)
+
+end # module
