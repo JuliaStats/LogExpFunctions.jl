8f6f530e000d789142659aab39b1c36a5d77e6fe
David Widmann <dev+github@devmotion.de>
2021-04-19 14:10:22 +0200

    Export constants and aliases as well
    Syncs list of exports with StatsFuns.

--- src/LogExpFunctions.jl
+++ src/LogExpFunctions.jl
@@ -1,11 +1,12 @@
 module LogExpFunctions
 
-export xlogx, xlogy, logistic, logit, log1psq, log1pexp, log1mexp, log2mexp, logexpm1,
-    log1pmx, logmxp1, logaddexp, logsubexp, logsumexp, softmax!, softmax
-
 using DocStringExtensions: SIGNATURES
-
 using Base: Math.@horner, @irrational
+
+export loghalf, logtwo, logπ, log2π, log4π
+export xlogx, xlogy, logistic, logit, log1psq, log1pexp, log1mexp, log2mexp, logexpm1,
+    softplus, invsoftplus, log1pmx, logmxp1, logaddexp, logsubexp, logsumexp, softmax,
+    softmax!
 
 include("constants.jl")
 include("basicfuns.jl")
