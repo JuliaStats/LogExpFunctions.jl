0eb71e15876eec4c0ece23c7729e026d6faab15d
David Widmann <devmotion@users.noreply.github.com>
2021-09-13 22:08:06 +0200

    Extend docstring
    Co-authored-by: Milan Bouchet-Valat <nalimilan@club.fr>

--- src/basicfuns.jl
+++ src/basicfuns.jl
@@ -256,6 +256,8 @@ Return the
 [softmax transformation](https://en.wikipedia.org/wiki/Softmax_function) of `x` over
 dimension `dims`.
 
+That is, return `exp.(x)`, normalized to sum to 1 over the given dimensions.
+
 See also: [`softmax!`](@ref)
 """
 softmax(x::AbstractArray{<:Real}; dims=:) =
