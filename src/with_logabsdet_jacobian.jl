9f1a2a755b0da957e137cf00f958c88a1fa098e7
Oliver Schulz <oschulz@mpp.mpg.de>
2021-11-08 21:12:42 +0100

    Add support for ChangesOfVariables.with_logabsdet_jacobian (#30)
    * Support ChangesOfVariables.with_logabsdet_jacobian
    
    * Document support for InverseFunctions and ChangesOfVariables
    
    * Neatify with_logabdet_jacobian defs and tests
    
    Co-authored-by: David Widmann <devmotion@users.noreply.github.com>
    
    * Fix with_logabsdet_jacobian
    
    Co-authored-by: David Widmann <devmotion@users.noreply.github.com>

---
+++ src/with_logabsdet_jacobian.jl
@@ -0,0 +1,29 @@
+function ChangesOfVariables.with_logabsdet_jacobian(::typeof(log1pexp), x::Real)
+    y = log1pexp(x)
+    return y, x - y
+end
+
+function ChangesOfVariables.with_logabsdet_jacobian(::typeof(logexpm1), x::Real)
+    y = logexpm1(x)
+    return y, x - y
+end
+
+function ChangesOfVariables.with_logabsdet_jacobian(::typeof(log1mexp), x::Real)
+    y = log1mexp(x)
+    return y, x - y
+end
+
+function ChangesOfVariables.with_logabsdet_jacobian(::typeof(log2mexp), x::Real)
+    y = log2mexp(x)
+    return y, x - y
+end
+
+function ChangesOfVariables.with_logabsdet_jacobian(::typeof(logit), x::Real)
+    y = logit(x)
+    y, -log(x * (1 - x))
+end
+
+function ChangesOfVariables.with_logabsdet_jacobian(::typeof(logistic), x::Real)
+    y = logistic(x)
+    y, log(y * (1 - y))
+end
---
+++ test/with_logabsdet_jacobian.jl
@@ -0,0 +1,19 @@
+@testset "with_logabsdet_jacobian" begin
+    derivative(f, x) = ChainRulesTestUtils.frule((ChainRulesTestUtils.NoTangent(), 1), f, x)[2]
+
+    x = randexp()
+
+    ChangesOfVariables.test_with_logabsdet_jacobian(log1pexp, x, derivative)
+    ChangesOfVariables.test_with_logabsdet_jacobian(log1pexp, -x, derivative)
+
+    ChangesOfVariables.test_with_logabsdet_jacobian(logexpm1, x, derivative)
+
+    ChangesOfVariables.test_with_logabsdet_jacobian(log1mexp, -x, derivative)
+
+    ChangesOfVariables.test_with_logabsdet_jacobian(log2mexp, log(2) - x, derivative)
+
+    ChangesOfVariables.test_with_logabsdet_jacobian(logistic, -x, derivative)
+    ChangesOfVariables.test_with_logabsdet_jacobian(logistic, x, derivative)
+
+    ChangesOfVariables.test_with_logabsdet_jacobian(logit, rand(), derivative)
+end
