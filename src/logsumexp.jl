1cf286c88a4408998b4ef71dc3ef448422a67b30
David Widmann <devmotion@users.noreply.github.com>
2022-03-12 20:31:47 +0100

    Update docstring

--- src/logsumexp.jl
+++ src/logsumexp.jl
@@ -34,10 +34,9 @@ logsumexp(X::AbstractArray{<:Number}; dims=:) = _logsumexp(X, dims)
 """
 $(SIGNATURES)
 
-Compute `out .= log.(sum!(out, exp.(X)))`.
+Compute [`logsumexp`](@ref) of `X` over the singleton dimensions of `out`, and write results to `out`.
 
-The result is computed in a numerically stable way that avoids
-intermediate over- and underflow using a single pass over the data.
+The result is computed in a numerically stable way that avoids intermediate over- and underflow using a single pass over the data.
 
 # References
 
