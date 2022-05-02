5747205dc38224bc16f971f43d7ad39a305392a3
Chad Scherrer <chad.scherrer@gmail.com>
2022-05-02 11:28:03 -0700

    more tests

--- test/sumlog.jl
+++ test/sumlog.jl
@@ -1,5 +1,7 @@
 @testset "sumlog" begin
-    for x in [10 .* rand(1000), repeat([nextfloat(1.0)], 1000), repeat([prevfloat(2.0)], 1000)]
-        @test (@inferred sumlog(x)) ≈ sum(log, x)
+    for T in [Int, Float16, Float32, Float64, BigFloat]
+        for x in [10 .* rand(1000), repeat([nextfloat(1.0)], 1000), repeat([prevfloat(2.0)], 1000)]
+            @test (@inferred sumlog(x)) ≈ sum(log, x)
+        end
     end
 end
