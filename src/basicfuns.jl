e663fe46698f09b2d66660417b761878bc73f855
Jishnu Bhattacharya <jishnub@users.noreply.github.com>
2022-05-11 01:00:07 +0400

    Fix whitespace in logit docstring (#51)
    * Fix whitespace in logit docstring
    
    Minor adjustment to spaces in the docstring of `logit`
    
    * Reformulate docstring
    
    Co-authored-by: David Widmann <david.widmann@it.uu.se>

--- src/basicfuns.jl
+++ src/basicfuns.jl
@@ -112,11 +112,11 @@ end
 """
 $(SIGNATURES)
 
-The [logit](https://en.wikipedia.org/wiki/Logit) or log-odds transformation,
-
+The [logit](https://en.wikipedia.org/wiki/Logit) or log-odds transformation, defined as
 ```math
-\\log\\left(\\frac{x}{1-x}\\right), \\text{where} 0 < x < 1
+\\operatorname{logit}(x) = \\log\\left(\\frac{x}{1-x}\\right)
 ```
+for ``0 < x < 1``.
 
 Its inverse is the [`logistic`](@ref) function.
 """
