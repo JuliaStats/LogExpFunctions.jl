9e38229e318525811198a2ccb276c8444698903b
David Widmann <devmotion@users.noreply.github.com>
2024-12-10 21:57:57 +0100

    Update Julia compat to 1.10

--- src/basicfuns.jl
+++ src/basicfuns.jl
@@ -237,7 +237,7 @@ function log1mexp(x::Real)
     if x < oftype(float(x), IrrationalConstants.loghalf)
         return log1p(-exp(x))
     else
-        return log(-_expm1(x))
+        return log(-expm1(x))
     end
 end
 
@@ -246,7 +246,7 @@ $(SIGNATURES)
 
 Return `log(2 - exp(x))` evaluated as `log1p(-expm1(x))`
 """
-log2mexp(x::Real) = log1p(-_expm1(x))
+log2mexp(x::Real) = log1p(-expm1(x))
 
 """
 $(SIGNATURES)
@@ -254,7 +254,7 @@ $(SIGNATURES)
 Return `log(exp(x) - 1)` or the “invsoftplus” function.  It is the inverse of
 [`log1pexp`](@ref) (aka “softplus”).
 """
-logexpm1(x::Real) = x <= 18.0 ? log(_expm1(x)) : x <= 33.3 ? x - exp(-x) : oftype(exp(-x), x)
+logexpm1(x::Real) = x <= 18.0 ? log(expm1(x)) : x <= 33.3 ? x - exp(-x) : oftype(exp(-x), x)
 logexpm1(x::Float32) = x <= 9f0 ? log(expm1(x)) : x <= 16f0 ? x - exp(-x) : oftype(exp(-x), x)
 
 const softplus = log1pexp
@@ -440,7 +440,7 @@ $(SIGNATURES)
 
 Compute the complementary double exponential, `1 - exp(-exp(x))`.
 """
-cexpexp(x) = -_expm1(-exp(x))
+cexpexp(x) = -expm1(-exp(x))
 
 #=
 this uses the identity:
